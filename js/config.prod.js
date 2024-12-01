0eb05b80f77868ed920d0c834867454f1ff8cbac
Jose Antonio Tapia <antapia3003@gmail.com>
2024-11-30 20:56:59 -0600

    Fix: mejora en manejo de autenticación

--- js/config.prod.js
+++ js/config.prod.js
@@ -1,7 +1,7 @@
 const config = {
     REPO_OWNER: 'AntonioIQ',
     REPO_NAME: 'quiniela-liguilla',
-    GITHUB_TOKEN: '' // Se llenará desde localStorage
+    GITHUB_TOKEN: null  // Será sobrescrito con el token del localStorage
 };
 
 export default config;
